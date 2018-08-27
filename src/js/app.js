25d3c94a7899d07fa0bf04e0f7f8dff5923902a1
helloyong <2628578405@qq.com>
2018-08-27 19:38:33 +0800

    添加登入登出功能

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,8 @@
-var app = new Vue({
+let app = new Vue({
   el: '#app',
   data: {
+    loginVisible: false,
+    signUpVisible: false,
     resume: {
       name: '姓名',
       gender: '男',
@@ -9,11 +11,58 @@ var app = new Vue({
       phone: '12345678901',
       email: 'example@example.com'
     },
-
+    login: {
+      email: '',
+      password: ''
+    },
+    signUp: {
+      email: '',
+      password: ''
+    },
   },
   methods: {
-    onEdit(key, value) {
+    onEdit(key, value){
       this.resume[key] = value
     },
+    onLogin(e){
+      AV.User.logIn(this.login.email, this.login.password).then(function (user) {
+        console.log(user)
+      }, function (error) {
+        if (error.code === 211 && error.code === 210) {
+          alert('邮箱和密码不匹配')
+        }
+      })
+    },
+    onLogout(e){
+      AV.User.logOut();
+      alert('注销成功')
+      window.location.reload()
+    },
+    onSignUp(e){
+      const user = new AV.User()
+      user.setUsername(this.signUp.email)
+      user.setPassword(this.signUp.password)
+      user.setEmail(this.signUp.email)
+      user.signUp().then(function (user) {
+        console.log(user)
+      }, function (error) {
+      })
+    },
+    onClickSave(){
+      let currentUser = AV.User.current()
+      if (!currentUser) {
+        this.loginVisible = true
+      } else {
+        this.saveResume()
+      }
+    },
+    saveResume(){
+      let {id} = AV.User.current()
+      let user = AV.Object.createWithoutData('User', id)
+      user.set('resume', this.resume)
+      user.save()
+    },
+
   }
 })
+
