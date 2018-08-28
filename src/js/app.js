e653030b060cfd88344215bf05139c1f79e9b356
helloyong <2628578405@qq.com>
2018-08-28 15:55:01 +0800

    完善登录, 其余部分编辑

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,33 @@
-let app = new Vue({
-  el: '#app',
+let app = new Vue({ el: '#app',
   data: {
     loginVisible: false,
     signUpVisible: false,
+    shareVisible: false,
+    previewUser: {
+      objectId: undefined,
+    },
+    previewResume: {},
+    currentUser: {
+      objectId: undefined,
+      email: '',
+    },
     resume: {
       name: '姓名',
-      gender: '男',
-      birthday: '1990-10-02',
+      gender: '女',
+      birthday: '1990年1月',
       jobTitle: '前端工程师',
-      phone: '12345678901',
-      email: 'example@example.com'
+      phone: '138111111111',
+      email: 'example@example.com',
+      skills: [
+        {name: '请填写技能名称', description: '请填写技能描述'},
+        {name: '请填写技能名称', description: '请填写技能描述'},
+        {name: '请填写技能名称', description: '请填写技能描述'},
+        {name: '请填写技能名称', description: '请填写技能描述'},
+      ],
+      projects: [
+        {name: '请填写项目名称', link: 'http://...', keywords: '请填写关键词', description: '请详细描述'},
+        {name: '请填写项目名称', link: 'http://...', keywords: '请填写关键词', description: '请详细描述'},
+      ]
     },
     login: {
       email: '',
@@ -19,16 +37,49 @@ let app = new Vue({
       email: '',
       password: ''
     },
+    shareLink: '不知道',
+    mode: 'edit' // 'preview'
+  },
+  computed: {
+    displayResume () {
+      return this.mode === 'preview' ? this.previewResume : this.resume
+    }
+  },
+  watch: {
+    'currentUser.objectId': function (newValue, oldValue) {
+      if (newValue) {
+        this.getResume(this.currentUser)
+      }
+    }
   },
   methods: {
     onEdit(key, value){
-      this.resume[key] = value
+      let regex = /\[(\d+)\]/g
+      key = key.replace(regex, (match, number) => `.${number}`)
+      // key = skills.0.name
+      keys = key.split('.')
+      let result = this.resume
+      for (let i = 0; i < keys.length; i++) {
+        if (i === keys.length - 1) {
+          result[keys[i]] = value
+        } else {
+          result = result[keys[i]]
+        }
+      }
+    },
+    hasLogin () {
+      return !!this.currentUser.objectId
     },
     onLogin(e){
-      AV.User.logIn(this.login.email, this.login.password).then(function (user) {
-        console.log(user)
-      }, function (error) {
-        if (error.code === 211 && error.code === 210) {
+      AV.User.logIn(this.login.email, this.login.password).then((user) => {
+        user = user.toJSON()
+        this.currentUser.objectId = user.objectId
+        this.currentUser.email = user.email
+        this.loginVisible = false
+      }, (error) => {
+        if (error.code === 211) {
+          alert('邮箱不存在')
+        } else if (error.code === 210) {
           alert('邮箱和密码不匹配')
         }
       })
@@ -43,9 +94,14 @@ let app = new Vue({
       user.setUsername(this.signUp.email)
       user.setPassword(this.signUp.password)
       user.setEmail(this.signUp.email)
-      user.signUp().then(function (user) {
-        console.log(user)
-      }, function (error) {
+      user.signUp().then((user) => {
+        alert('注册成功')
+        user = user.toJSON()
+        this.currentUser.objectId = user.objectId
+        this.currentUser.email = user.email
+        this.signUpVisible = false
+      }, (error) => {
+        alert(error.rawMessage)
       })
     },
     onClickSave(){
@@ -57,12 +113,63 @@ let app = new Vue({
       }
     },
     saveResume(){
-      let {id} = AV.User.current()
-      let user = AV.Object.createWithoutData('User', id)
+      let {objectId} = AV.User.current().toJSON()
+      let user = AV.Object.createWithoutData('User', objectId)
       user.set('resume', this.resume)
-      user.save()
+      user.save().then(() => {
+        alert('保存成功')
+      }, () => {
+        alert('保存失败')
+      })
     },
-
+    getResume (user) {
+      var query = new AV.Query('User');
+      return query.get(user.objectId).then((user) => {
+        let resume = user.toJSON().resume
+        return resume
+      }, (error) => {
+        // 异常处理
+      });
+    },
+    addSkill () {
+      this.resume.skills.push({name: '请填写技能名称', description: '请填写技能描述'})
+    },
+    removeSkill (index) {
+      this.resume.skills.splice(index, 1)
+    },
+    addProject () {
+      this.resume.projects.push(
+        {name: '请填写项目名称', link: 'http://...', keywords: '请填写关键词', description: '请详细描述'},
+      )
+    },
+    removeProject (index) {
+      this.resume.projects.splice(index, 1)
+    },
   }
 })
+
+
+// 获取当前用户
+let currentUser = AV.User.current()
+if (currentUser) {
+  app.currentUser = currentUser.toJSON()
+  app.shareLink = location.origin + location.pathname + '?user_id=' + app.currentUser.objectId
+  app.getResume(app.currentUser).then(resume => {
+    app.resume = resume
+  })
+}
+
+
+// 获取预览用户的 id
+let search = location.search
+let regex = /user_id=([^&]+)/
+let matches = search.match(regex)
+let userId
+if (matches) {
+  userId = matches[1]
+  app.mode = 'preview'
+  app.getResume({objectId: userId}).then(resume => {
+    app.previewResume = resume
+  })
+}
 
